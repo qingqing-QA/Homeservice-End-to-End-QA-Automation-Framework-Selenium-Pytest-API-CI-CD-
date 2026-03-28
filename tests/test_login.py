ac0a1f1173e0dcf4207d989f45a90c41eb01be25
qingqing-Na <narenbnu@gmail.com>
2026-03-28 00:23:12 -0400

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,6 +4,6 @@ def test_login(driver):
     driver.get("http://localhost/database_javascript/project1/Frontend/index.html")
 
     login_page = LoginPage(driver)
-    login_page.login("testuser", "password")
+    login_page.login("admin", "admin123")
 
     assert "dashboard" in driver.current_url
