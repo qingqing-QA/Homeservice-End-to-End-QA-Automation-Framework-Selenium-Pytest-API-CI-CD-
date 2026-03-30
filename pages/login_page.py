a161affee6bb343b5d94645926f4a6e35f67e521
qingqing-Na <narenbnu@gmail.com>
2026-03-29 22:32:58 -0400

    Import LoginPage in login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from pages.login_page import LoginPage
 
 class LoginPage(BasePage):
     USERNAME = (By.ID, "admin")
